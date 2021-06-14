32cd7977057f76bfadafac17de86609ba3952d3a
Fogarty <sfogarty@rome0011.slac.stanford.edu>
2021-06-14 14:47:15 -0700

    added setup file for larndsim

---
+++ larnd-sim_scripts/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import setuptools
+
+VER = "0.1.0"
+
+setuptools.setup(
+    name="larndsim",
+    version=VER,
+    author="DUNE collaboration",
+    author_email="roberto@lbl.gov",
+    description="Simulation framework for the DUNE LArND",
+    url="https://github.com/DUNE/larnd-sim",
+    packages=setuptools.find_packages(),
+    install_requires=["numpy", "pytest", "numba==0.52", "larpix-control", "larpix-geometry", "tqdm", "fire", "cupy"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: by End-User Class :: Developers",
+        "Operating System :: Grouping and Descriptive Categories :: OS Independent (Written in an interpreted language)",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Physics"
+    ],
+    python_requires='>=3.7',
+)
